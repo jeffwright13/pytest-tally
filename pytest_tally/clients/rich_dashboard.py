d4f0ae7b7a17d7059d77c52981dd90eb4256fa73
Jeffrey Wright <jeffrey.wright@wartsila.com>
2023-03-05 22:50:11 -0700

    Introduce duration. Update Rich dashboard. Cleanup source files w/ pyflakes, isort, black.

--- pytest_tally/clients/rich_dashboard.py
+++ pytest_tally/clients/rich_dashboard.py
@@ -14,6 +14,15 @@ class Duration(Quantity):
 
 from pytest_tally.plugin import FILE, TestSessionData
 
+NULL_TEST_SESSION_DATA = TestSessionData(
+    num_collected_tests=0,
+    start_time=None,
+    end_time=None,
+    total_duration=0,
+    reports={},
+    session_finished=False,
+)
+
 
 def get_test_session_data() -> TestSessionData:
     try:
@@ -21,36 +30,32 @@ def get_test_session_data() -> TestSessionData:
             try:
                 j = json.load(jfile)
             except json.decoder.JSONDecodeError:
-                # print("JSON decode error encountered during get_test_session_data() - returning null test_session_data.")
-                return TestSessionData(
-                    num_collected_tests=0,
-                    total_duration=0,
-                    reports={},
-                    session_finished=False,
-                )
-        test_session_data = TestSessionData.from_json(j)
-        return test_session_data
+                return NULL_TEST_SESSION_DATA
+        return TestSessionData.from_json(j)
+
     except (FileNotFoundError, EOFError):
-        print("File not found error encountered during get_test_session_data() - returning null test_session_data.")
-        return TestSessionData(
-            num_collected_tests=0,
-            total_duration=0,
-            reports={},
-            session_finished=False,
+        print(
+            "File not found error encountered during get_test_session_data() - returning null test_session_data."
         )
+        return NULL_TEST_SESSION_DATA
 
 
 def generate_table() -> Table:
+    test_session_data = get_test_session_data()
+
     table = Table(highlight=True)
     table.add_column("Test NodeId")
     table.add_column("Duration")
     table.add_column("Outcome")
-
-    test_session_data = get_test_session_data()
+    table.caption = f"[b]Total Test Session Duration: {Duration(test_session_data.total_duration)}[/b]"
 
     for report in test_session_data.reports:
         name = Text(test_session_data.reports[report]["node_id"], style="bold cyan")
-        duration = Duration(str(test_session_data.reports[report]["duration"])) if test_session_data.reports[report]["duration"] else "-"
+        duration = (
+            Duration(str(test_session_data.reports[report]["duration"]))
+            if test_session_data.reports[report]["duration"]
+            else "-"
+        )
         outcome = Text(test_session_data.reports[report]["modified_outcome"])
         if "passed" in outcome.plain.lower():
             outcome.stylize("green")
@@ -62,29 +67,27 @@ def generate_table() -> Table:
             outcome.stylize("magenta")
         else:
             outcome.stylize("blue")
+        table.add_row(name, render(duration, "s"), outcome)
 
-        table.add_row(name, render(duration, 's'), outcome)
     return table
 
 
 def main():
-    table = generate_table()
+    # table = generate_table()
     while True:
-        # print("True loop 1")
         test_session_data = get_test_session_data()
         with Live(generate_table(), refresh_per_second=4) as live:
             while not test_session_data.session_finished:
-                # print("True loop 2")
                 time.sleep(0.4)
                 live.update(generate_table())
                 test_session_data = get_test_session_data()
 
         while True:
-            # print("True loop 3")
             test_session_data = get_test_session_data()
             if not test_session_data.session_finished:
                 break
-            time.sleep(2)
+            time.sleep(0.5)
+
 
 if __name__ == "__main__":
     main()
--- pytest_tally/plugin.py
+++ pytest_tally/plugin.py
@@ -4,18 +4,14 @@ from datetime import datetime, timezone
 from pathlib import Path
 
 import pytest
-from _pytest._io.terminalwriter import TerminalWriter
-from _pytest.config import Config, create_terminal_writer
+from _pytest.config import Config, ExitCode
+from _pytest.main import Session
 from _pytest.nodes import Item
-from _pytest.reports import TestReport
+from _pytest.runner import CallInfo
+from count_timer import CountTimer
 from dataclasses_json import dataclass_json
-from rich.live import Live
-from rich.progress import Progress
-from rich.table import Table
 
 FILE = Path("/Users/jwr003/coding/pytest-tally/pytest_tally/data.json")
-
-from dataclasses import dataclass
 
 
 @dataclass_json
@@ -30,9 +26,15 @@ class TestReportDistilled:
 @dataclass
 class TestSessionData:
     num_collected_tests: int
+    start_time: str
+    end_time: str
     total_duration: float
     reports: dict
     session_finished: bool
+
+
+def check_tally_enabled(config: Config) -> bool:
+    return bool(config.option.tally) if hasattr(config.option, "tally") else False
 
 
 def pytest_addoption(parser) -> None:
@@ -40,33 +42,53 @@ def pytest_addoption(parser) -> None:
     group.addoption(
         "--tally",
         action="store_true",
-        help="Enable the pytest-tally plugin. Generates a live-updating table of test results in the terminal.",
+        help="Enable the pytest-tally plugin. Writes live summary results data to a JSON file for comsumption by a dashboard client.",
     )
 
 
-def pytest_sessionstart(session):
-    if not hasattr(session.config.option, "tally"):
+def pytest_cmdline_main(config: Config) -> None:
+    if not check_tally_enabled(config):
+        return
+
+    config._tally_session_start_time = (
+        datetime.now(timezone.utc).replace(microsecond=0).strftime("%Y-%m-%d %H:%M:%S")
+    )
+    config._tally_timer = CountTimer()
+    config._tally_timer.start()
+
+
+def pytest_sessionstart(session: Session) -> None:
+    if not check_tally_enabled(session.config):
         return
 
     session.test_session_data = TestSessionData(
-        total_duration=0, num_collected_tests=0, reports={}, session_finished=False
+        num_collected_tests=0,
+        start_time=None,
+        end_time=None,
+        total_duration=0,
+        reports={},
+        session_finished=False,
     )
-    with open(FILE, "w", encoding='utf-8') as file:
+    with open(FILE, "w", encoding="utf-8") as file:
         j = session.test_session_data.to_json()
         json.dump(j, file)
 
 
-def pytest_collection_finish(session):
-    if not hasattr(session.config.option, "tally"):
+def pytest_collection_finish(session: Session) -> None:
+    if not check_tally_enabled(session.config):
         return
 
     session.test_session_data = TestSessionData(
-        total_duration=0, num_collected_tests=0, reports={}, session_finished=False
+        num_collected_tests=0,
+        start_time=None,
+        end_time=None,
+        total_duration=0,
+        reports={},
+        session_finished=False,
     )
     with open(FILE, "w") as file:
         j = session.test_session_data.to_json()
         json.dump(j, file)
-
 
     session.test_session_data.num_collected_tests = len(session.items)
     with open(FILE, "w") as file:
@@ -75,44 +97,58 @@ def pytest_collection_finish(session):
 
 
 @pytest.hookimpl(hookwrapper=True)
-def pytest_runtest_makereport(item, call):
-    if not hasattr(item.session.config.option, "tally"):
+def pytest_runtest_makereport(item: Item, call: CallInfo) -> None:
+    if not check_tally_enabled(item.session.config):
+        yield
+    else:
+
+        def _process_outcome(report):
+            if report.when in ["setup", "teardown"] and report.outcome == "failed":
+                return "Error"
+            if hasattr(report, "wasxfail"):
+                if report.outcome in ["passed", "failed"]:
+                    return "XPassed"
+                if report.outcome == "skipped":
+                    return "XFailed"
+            return report.outcome.capitalize()
+
+        outcome = yield
+        report = outcome.get_result()
+        modified_outcome = _process_outcome(report)
+        distilled = TestReportDistilled(
+            duration=report.duration,
+            modified_outcome=modified_outcome,
+            node_id=report.nodeid,
+        )
+        item.session.test_session_data.reports[item.name] = distilled
+        item.session.test_session_data.total_duration = (
+            item.session.config._tally_timer.elapsed
+        )
+        with open(FILE, "w") as file:
+            j = item.session.test_session_data.to_json()
+            json.dump(j, file)
+
+
+def pytest_sessionfinish(session: Session, exitstatus: ExitCode) -> None:
+    if not check_tally_enabled(session.config):
         return
 
-    def _process_outcome(report):
-        if report.when in ["setup", "teardown"] and report.outcome == "failed":
-            return "Error"
-        if hasattr(report, "wasxfail"):
-            if report.outcome in ["passed", "failed"]:
-                return "XPassed"
-            if report.outcome == "skipped":
-                return "XFailed"
-        return report.outcome.capitalize()
+    session.config._tally_timer.pause()
 
-    outcome = yield
-    report = outcome.get_result()
-    modified_outcome = _process_outcome(report)
-    distilled = TestReportDistilled(
-        duration=report.duration,
-        modified_outcome=modified_outcome,
-        node_id=report.nodeid,
-    )
-    item.session.test_session_data.reports[item.name] = distilled
-    with open(FILE, "w") as file:
-        j = item.session.test_session_data.to_json()
-        json.dump(j, file)
-
-
-def pytest_sessionfinish(session, exitstatus):
-    if not hasattr(session.config.option, "tally"):
-        return
-
-    session.test_session_data.total_duration = sum(
-        session.test_session_data.reports[report].duration
-        for report in session.test_session_data.reports
-    )
-    # session.test_session_data.total_duration = session.duration
+    session.test_session_data.total_duration = session.config._tally_timer.elapsed
     session.test_session_data.session_finished = True
     with open(FILE, "w") as file:
         j = session.test_session_data.to_json()
         json.dump(j, file)
+
+
+def pytest_unconfigure(config: Config) -> None:
+    if not check_tally_enabled(config):
+        return
+
+    config._tally_session_end_time = (
+        datetime.now(timezone.utc).replace(microsecond=0).strftime("%Y-%m-%d %H:%M:%S")
+    )
+    config._tally_session_duration = datetime.strptime(
+        config._tally_session_end_time, "%Y-%m-%d %H:%M:%S"
+    ) - datetime.strptime(config._tally_session_start_time, "%Y-%m-%d %H:%M:%S")
